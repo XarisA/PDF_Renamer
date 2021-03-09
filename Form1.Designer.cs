a1f3dcdb535888dbdbf1c2a8d7c3579028d1ce33
Xaris Ar <xaris@tuta.io>
2021-03-09 16:13:11 +0200

    Added Project Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace PDFR
+{
+    partial class Main_frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_frm));
+            this.openfile_btn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rename_btn = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ifra_chk = new System.Windows.Forms.RadioButton();
+            this.msds_chk = new System.Windows.Forms.RadioButton();
+            this.recurse_chk = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openfile_btn
+            // 
+            this.openfile_btn.Image = ((System.Drawing.Image)(resources.GetObject("openfile_btn.Image")));
+            this.openfile_btn.Location = new System.Drawing.Point(380, 26);
+            this.openfile_btn.Name = "openfile_btn";
+            this.openfile_btn.Size = new System.Drawing.Size(35, 30);
+            this.openfile_btn.TabIndex = 1;
+            this.openfile_btn.UseVisualStyleBackColor = true;
+            this.openfile_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(362, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose folder";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox2.Location = new System.Drawing.Point(58, 210);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(94, 25);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(12, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prefix";
+            // 
+            // rename_btn
+            // 
+            this.rename_btn.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.rename_btn.Image = ((System.Drawing.Image)(resources.GetObject("rename_btn.Image")));
+            this.rename_btn.Location = new System.Drawing.Point(328, 203);
+            this.rename_btn.Name = "rename_btn";
+            this.rename_btn.Size = new System.Drawing.Size(87, 36);
+            this.rename_btn.TabIndex = 4;
+            this.rename_btn.Text = "Rename";
+            this.rename_btn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.rename_btn.UseVisualStyleBackColor = true;
+            this.rename_btn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label3.Location = new System.Drawing.Point(171, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Suffix";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox3.Location = new System.Drawing.Point(216, 210);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(94, 25);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label4.Location = new System.Drawing.Point(9, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "made with .Net and ♡ by xaris";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ifra_chk);
+            this.groupBox1.Controls.Add(this.msds_chk);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 128);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(403, 66);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose PDF type";
+            // 
+            // ifra_chk
+            // 
+            this.ifra_chk.AutoSize = true;
+            this.ifra_chk.Location = new System.Drawing.Point(92, 25);
+            this.ifra_chk.Name = "ifra_chk";
+            this.ifra_chk.Size = new System.Drawing.Size(167, 21);
+            this.ifra_chk.TabIndex = 1;
+            this.ifra_chk.TabStop = true;
+            this.ifra_chk.Text = "IFRA - Allergens - NMPP";
+            this.ifra_chk.UseVisualStyleBackColor = true;
+            // 
+            // msds_chk
+            // 
+            this.msds_chk.AutoSize = true;
+            this.msds_chk.Location = new System.Drawing.Point(6, 25);
+            this.msds_chk.Name = "msds_chk";
+            this.msds_chk.Size = new System.Drawing.Size(61, 21);
+            this.msds_chk.TabIndex = 0;
+            this.msds_chk.TabStop = true;
+            this.msds_chk.Text = "MSDS";
+            this.msds_chk.UseVisualStyleBackColor = true;
+            // 
+            // recurse_chk
+            // 
+            this.recurse_chk.AutoSize = true;
+            this.recurse_chk.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.recurse_chk.Location = new System.Drawing.Point(6, 24);
+            this.recurse_chk.Name = "recurse_chk";
+            this.recurse_chk.Size = new System.Drawing.Size(186, 21);
+            this.recurse_chk.TabIndex = 9;
+            this.recurse_chk.Text = "Recurse into subdirectories";
+            this.recurse_chk.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.recurse_chk);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 62);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(403, 60);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Options";
+            // 
+            // Main_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 272);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rename_btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.openfile_btn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Main_frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF Renamer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openfile_btn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button rename_btn;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox recurse_chk;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton ifra_chk;
+        private System.Windows.Forms.RadioButton msds_chk;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFR
+{
+    public partial class Main_frm : Form
+    {
+        public Main_frm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.textBox1.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string folderp = textBox1.Text;
+
+                if (Directory.Exists(folderp))
+                {
+                    try
+                    {
+                        ProcessDirectory(folderp);
+                        MessageBox.Show("Rename Completed");
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Can not parse PDF. Duplicate or unsuported type.");
+                        throw;
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Choose a folder first! Not a valid directory");
+                    Debug.WriteLine("{0} is not a valid directory.", folderp);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        public void ProcessDirectory(string directory)
+        {
+            string[] fileEntries = Directory.GetFiles(directory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            if (recurse_chk.Checked) 
+            {
+                string[] subdirectories = Directory.GetDirectories(directory);
+                foreach (string dir in subdirectories)
+                    ProcessDirectory(dir);
+            }
+        }
+
+        public void ProcessFile(string path)
+        {
+            string filename = System.IO.Path.GetFileNameWithoutExtension(path);
+            if (System.IO.Path.GetExtension(path) == ".pdf")
+            {
+                string descr = String.Empty;
+                descr = ExtractTextFromPdf(path);
+                string newname =  textBox2.Text + " " + descr + " " + textBox3.Text + ".pdf";
+                string newpath = System.IO.Path.GetDirectoryName(path) + "\\" + newname;
+                File.Copy(path, newpath);
+                Debug.WriteLine(path);
+                Debug.WriteLine(newpath);
+                Debug.WriteLine("Processed file '{0}'.", newpath);
+            }
+        }
+        public string ExtractTextFromPdf(string path)
+        {
+            int jump = 0;
+            if (ifra_chk.Checked) { jump = 3; }
+            if (msds_chk.Checked) { jump = 9; }
+            using (PdfReader reader = new PdfReader(path))
+            {
+                StringBuilder text = new StringBuilder();
+                ITextExtractionStrategy Strategy = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    string page = "";
+
+                    page = PdfTextExtractor.GetTextFromPage(reader, i, Strategy);
+                    string[] lines = page.Split('\n');
+
+                    foreach (string line in lines.Skip(jump))
+                    {
+                        var start = line.IndexOf(": ")+2;
+                        var match2 = line.Substring(start, line.IndexOf("/") - start);
+                        Debug.WriteLine(match2);
+                        return match2;
+                    }
+                }
+            }
+            return String.Empty;
+        }
+    }
+}
